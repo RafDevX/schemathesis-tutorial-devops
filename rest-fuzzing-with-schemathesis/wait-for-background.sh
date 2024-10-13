d08007d25ab6301102476232534165755335b703
Rafael Oliveira <rmfseo@kth.se>
2024-10-13 19:48:30 +0200

    clear terminal in bash spinner
    Since the script is running in the foreground, all commands are visible
    to the user, so there is no need to carefully erase a specific line - it
    is necessary to erase the entire screen.

--- rest-fuzzing-with-schemathesis/wait-for-background.sh
+++ rest-fuzzing-with-schemathesis/wait-for-background.sh
@@ -1,17 +1,16 @@
 # spinner adapted from https://stackoverflow.com/a/3330834
 
-clear
-
 while true; do
     for s in / - \\ \|; do
-        printf "\r$s Loading..."
+        clear
+        echo "$s Loading..."
         sleep 0.1
 
         if [ -f /tmp/bg-ready ]; then
-            printf "\r\e[K" # clear line
             break 2
         fi
     done
 done
 
 rm /tmp/bg-ready # prepare for next invocation
+clear
